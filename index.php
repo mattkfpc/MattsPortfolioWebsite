a0a8343185c7f232a4c83c8a5e8f60abe3de8bab
Matthew <matt.kfpc@gmail.com>
2017-06-08 00:49:38 -0400

    updated

--- index.php
+++ index.php
@@ -106,7 +106,10 @@ if($_POST["submit"]) {
 				<li><a href="/Website1" target="_blank"><img src="http://i.imgur.com/ShG8vFM.png" alt="First Website Portfolio"/></a><p>My First Website</p></li>
 
         <!--project-->
-				<li><a href="/musclegame.html"><img src="http://i.imgur.com/kcXUl63.png" alt="Muscle Game Wireframe"/></a><p>My First Website</p></li>
+				<li><a href="/musclegame.html"><img src="http://i.imgur.com/kcXUl63.png" alt="Muscle Game Wireframe"/></a><p>Muscle Game Wireframe</p></li>
+
+        <!--project-->
+				<li><a href="/paekfamily.html"><img src="http://i.imgur.com/Ue2s8O6.png" alt="Family Website Wireframe"/></a><p>Family Website Wireframe</p></li>
 
         <!--project-->
         <li><a href="http://www.nfdrapery.com" target="_blank"><img src="http://i.imgur.com/cnLo101.png" alt="NFDrapery Website"/></a><p>NFDrapery Portfolio (client)</p></li>
